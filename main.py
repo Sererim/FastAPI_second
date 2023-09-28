c1a1cef8517e3561623ea6cfc9e90e00ba5842fd
Sererim <danielleymanw@gmail.com>
2023-09-28 14:38:02 +0300

    Models are ready, tables soon to come.

--- main.py
+++ main.py
@@ -1,7 +1,47 @@
+import databases
+import sqlalchemy
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
+
+DATABASE_URL: str = "sqlite:///shop.db"
+database = databases.Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
+
+class Tables():
+    
+    users = sqlalchemy.Table(
+        "Users",
+        metadata,
+        sqlalchemy.Column("id", sqlalchemy.INTEGER, primary_key=True),
+        sqlalchemy.Column("firstname", sqlalchemy.String(length=45)),
+        sqlalchemy.Column("secondname", sqlalchemy.String(length=45)),
+        sqlalchemy.Column("username", sqlalchemy.String(45)),
+        sqlalchemy.Column("email", sqlalchemy.String()),
+        sqlalchemy.Column("password", sqlalchemy.String())
+    )
+    
+    products = sqlalchemy.Table(
+        "Products",
+        metadata,
+        sqlalchemy.Column("id", sqlalchemy.INTEGER, primary_key=True),
+        sqlalchemy.Column("name of the product", sqlalchemy.String()),
+        sqlalchemy.Column("description of the product", sqlalchemy.TEXT()),
+        sqlalchemy.Column("product price", sqlalchemy.DECIMAL(precision=2))
+    )
+
+    orders = sqlalchemy.Table(
+        "Orders",
+        metadata,
+        sqlalchemy.Column("id", sqlalchemy.INTEGER, primary_key=True),
+        sqlalchemy.Column("user_id", sqlalchemy.INTEGER, foreign_key="Users.id"),
+        sqlalchemy.Column("product_id", sqlalchemy.INTEGER, foreign_key="Products.id"),
+        sqlalchemy.Column()
+    )
+
+engine = sqlalchemy.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+metadata.create_all(engine)
 
 app = FastAPI()
 
--- model.py
+++ model.py
@@ -1,3 +1,69 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
+from decimal import Decimal
+from datetime import datetime
 
+
+class UserIn(BaseModel):
+    """
+    Model for input data for user table.
+    """
+    firstname: str = Field(..., title="First name.", min_length=2, max_length=45)
+    secondname: str = Field(..., title="Second name.", min_length=2, max_length=45)
+    username: str = Field(..., title="User name", min_length=3, max_length=45)
+    email: str = Field(..., title="User email", min_length=8)
+    password: str = Field(..., title="User password.", min_length=8)
+    
+
+class UserOut(BaseModel):
+    """
+    Model for output data for user table.
+    """
+    id: int = Field(..., title="User id.", gt=0)
+    firstname: str = Field(..., title="First name.", min_length=2, max_length=45)
+    secondname: str = Field(..., title="Second name.", min_length=2, max_length=45)
+    username: str = Field(..., title="User name", min_length=3, max_length=45)
+    email: str = Field(..., title="User email", min_length=8)
+    password: str = Field(..., title="User password.", min_length=8)
+
+
+class ProductIn(BaseModel):
+    """
+    Model for product input.
+    """
+    product_name: str = Field(..., title="Name of the product")
+    description: str = Field(..., title="Description of the product.")
+    price: Decimal = Field(..., title="Price of the product.", gt=0.00, decimal_places=2, description="Must be greater than zero.")
+    
+
+class ProductOut(BaseModel):
+    """
+    Model for product output.
+    Args:
+        BaseModel (_type_): _description_
+    """
+    id: int = Field(..., title="Product id.", gt=0)
+    product_name: str = Field(..., title="Name of the product")
+    description: str = Field(..., title="Description of the product.")
+    price: Decimal = Field(..., title="Price of the product.", gt=0.00, decimal_places=2, description="Must be greater than zero.")
+
+
+class OrderIn(BaseModel):
+    """
+    Model for order input.
+    Args:
+        BaseModel (_type_): _description_
+    """
+    user_id: int = Field(..., title="Product id.", gt=0)
+    product_id: int = Field(..., title="Product id.", gt=0)
+    date: datetime = Field(..., gt= datetime.now())
+
+
+class OrderOut(BaseModel):
+    """
+    Model for order output.
+    """
+    id: int = Field(..., title="Product id.", gt=0)
+    user_id: int = Field(..., title="Product id.", gt=0)
+    product_id: int = Field(..., title="Product id.", gt=0)
+    date: datetime = Field(..., gt= datetime.now())
